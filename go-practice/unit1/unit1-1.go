dc19178d882ce9a7a85509f45fffc487fd9d20f8
김영배 <canoe918@gmail.com>
2025-08-28 13:12:05 +0900

    feat: go grammar

--- go-practice/unit1/unit1-1.go
+++ go-practice/unit1/unit1-1.go
@@ -5,4 +5,21 @@ import "fmt"
 func main() {
 	value := 10
 	fmt.Println(value)
+
+	i := 10
+
+	if i >= 5 {
+		fmt.Println("i >= 5")
+	}
+
+	for i := 0; i < 5; i++ {
+		fmt.Println()
+	}
+
+	// comments
+
+	/*
+	multiline
+	comments
+	*/
 }
---
+++ go-practice/unit1/unit1-2.go
@@ -0,0 +1,5 @@
+package main
+
+var a int = 1;
+var b float32 = 1.3;
+// var c = a + b;
